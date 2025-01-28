0a119171375610ed08d7834ef4565f1868d4d7a9
unknown <v.varshith.2004@gmail.com>
2025-01-28 20:13:59 +0530

    update

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def serve_file(filename):
 if __name__ == "__main__":
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
